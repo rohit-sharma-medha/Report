fa8a557b640af7c9c697a762cacf3bcb0b995eab
Deepak Sharma <deepak@aayworks.com>
2023-03-08 14:05:00 +0530

    second orbit semi circle

--- src/components/seondCircle/SecondCircleUi.js
+++ src/components/seondCircle/SecondCircleUi.js
@@ -47,8 +47,102 @@ const SecondCircleUi = function SecondCircleUi() {
 
   return (
     <Fragment>
+        <div className='container row flex-column justify-content-around'>
+            <div className='row'>
+                <div className='col-12'>
+                        <div className='row align-items-center'>
+                            <div className='col-6 main_heading'>
+                            Make vocational education aspirational in UP
+                            </div>
+                            <div className='col-6 headingPara'>
+                            We renewed our 8-year-old partnership with the Department of Technical Education, Uttar Pradesh (DTE-UP) to strengthen polytechnic education, by:
+                            </div>
+                        </div>
+                </div>
 
-        <div className='container'>
+                </div>
+
+     
+            <div className='row circle_container'>
+                    <MySVG className="img-fluid semi-circle-svg-img" id="ellip_svg"/>
+                    <div className='row'>
+                            <div className='col-12' style={{zIndex:1}}>
+                                <div className='row justify-content-center mt-5'>
+                                    <div className='col-6 row justify-content-center align-items-center text-center second_grayed_bubble' id='sBubble'>
+                                        <div>
+                                        Building strong industry partnerships for quality OJTs
+                                        </div>
+                                    
+                                    </div>
+                                </div>
+
+                                <div className='row justify-content-center second_row'>
+                                    <div className='row justify-content-between'>
+                                        <div className='col-4 row justify-content-center align-items-center text-center second_grayed_bubble' id='fBubble'>
+                                            <div>
+                                                    Introducing an industry-relevant curriculum,
+
+                                            </div>
+                                   
+                                        </div>
+                                        <div className='col-4 row justify-content-center align-items-center text-center second_grayed_bubble' id='thBubble'>
+                                        Helping industry and academia work closely
+                                        </div>
+
+                                    </div>
+                                    
+                                </div>
+
+                                <div>
+                                    <div className='row justify-content-center'>
+                                        
+                                        <div className='col-6 row justify-content-center align-items-center text-center green_circle' id='foBubble'>
+                                            <div>
+                                                <div className='row justify-content-center' 
+                                                    style={{
+                                                        color:" #FFFFFF",
+                                                        fontSize: "1.5rem" ,
+                                                        fontWeight:700
+
+                                                    }}>
+                                                        Potentially impacting
+                                                    </div>
+                                                <div className='row justify-content-center'
+                                                style={{
+                                                    color:"#FFB900",
+                                                    fontSize: "4rem" ,
+                                                    fontWeight:400
+
+                                                }}>
+                                                    XXXX
+                                                </div>
+                                                <div className='row justify-content-center' 
+                                                style={{
+                                                    color:" #FFFFFF",
+                                                    fontSize: "1.5rem",
+                                                    fontWeight:700
+
+                                                }}>
+                                                    of students
+                                                </div>
+
+                                            </div>
+                                       
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            
+
+                            </div>
+                        </div>
+
+                </div> 
+
+
+        </div>
+
+        {/* <div className='container'>
             <div className='row'>
                <div className='col-12'>
                     <div className='row align-items-center'>
@@ -71,13 +165,13 @@ const SecondCircleUi = function SecondCircleUi() {
         <div className='row'>
             <div className='col-12'>
                 <div className='row justify-content-center'>
-                    <div className='col-4 second_grayed_bubble' id='sBubble'>
+                    <div className='col-4 second_' id='sBubble'>
                         Building strong industry partnerships for quality OJTs
                     </div>
 
                 </div>
                 <div className='row justify-content-between' >
-                    <div className='col-4 second_grayed_bubble' id='fBubble'>
+                    <div className='col-4 second_' id='fBubble'>
                     Providing on-the-job trainings (OJT) of 6 months’ duration
 
                     </div>
@@ -124,7 +218,7 @@ const SecondCircleUi = function SecondCircleUi() {
 
           
 
-        </div>
+        </div> */}
         
         </Fragment>
   )
