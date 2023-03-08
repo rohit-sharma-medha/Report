0c3b9c8ce97656084ae0012398b4e9d369be7007
Deepak Sharma <deepak@aayworks.com>
2023-03-08 17:03:05 +0530

    on screen rendering

--- src/components/seondCircle/SecondCircleUi.js
+++ src/components/seondCircle/SecondCircleUi.js
@@ -1,5 +1,5 @@
 
-import React,{Fragment,useEffect} from 'react';
+import React,{Fragment,useEffect,useState,useRef} from 'react';
 
 import {ReactComponent as MySVG} from '../../Assets/Images/Ellipse.svg';
 
@@ -9,9 +9,41 @@ import './SecondCircle.css';
 
 const SecondCircleUi = function SecondCircleUi() {
 
+    const [isVisible, setIsVisible] = useState(false);
+    const ref = useRef(null);
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.unobserve(entry.target);
+          }
+        },
+        {
+          root: null,
+          rootMargin: '0px',
+          threshold: 1.0,
+        }
+      );
+  
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+  
+      return () => {
+        if (ref.current) {
+        
+          observer.unobserve(ref.current);
+        }
+      };
+    }, []);
+
     const addAnimation = ()=>{
         const elementIdList = ['fBubble','sBubble','thBubble','foBubble'];
 
+        if(isVisible){
+            
         setTimeout(()=>{
             document.getElementById(elementIdList[0]).style.animation = "fadeIn 2s forwards"
 
@@ -35,20 +67,19 @@ const SecondCircleUi = function SecondCircleUi() {
             document.getElementById('ellip_svg').style.animation = "fadeInFromBottom 2s forwards"
         },9000)
 
-
+        }
     }
 
     useEffect(()=>{
         addAnimation()
 
-    },[])
-
-
+    },[isVisible])
 
 
   return (
-    <Fragment>
-        <div className='container row flex-column justify-content-around'>
+    <div ref={ref}>
+        
+        {isVisible?<div className='container row flex-column justify-content-around'>
             <div className='row'>
                 <div className='col-12'>
                         <div className='row align-items-center'>
@@ -141,87 +172,9 @@ const SecondCircleUi = function SecondCircleUi() {
                 </div> 
 
 
-        </div>
-
-        {/* <div className='container'>
-            <div className='row'>
-               <div className='col-12'>
-                    <div className='row align-items-center'>
-                        <div className='col-6 main_heading'>
-                        Elevate on-the-job vocational training in Haryana
-                        </div>
-                        <div className='col-6 headingPara'>
-                        Over the next 2 years, we will help the Skill Development and Industrial Training Department, Haryana (SDIT-Haryana) scale their dual-system of training model for vocational education students by:
-                        </div>
-                    </div>
-               </div>
-
-            </div>
-
-     
-
-     <div className='row justify-content-center'>
-        <MySVG className="img-fluid ellipse-svg" id='ellip_svg'/>
-
-        <div className='row'>
-            <div className='col-12'>
-                <div className='row justify-content-center'>
-                    <div className='col-4 second_' id='sBubble'>
-                        Building strong industry partnerships for quality OJTs
-                    </div>
-
-                </div>
-                <div className='row justify-content-between' >
-                    <div className='col-4 second_' id='fBubble'>
-                    Providing on-the-job trainings (OJT) of 6 months’ duration
-
-                    </div>
-                    <div className='col-4 second_grayed_bubble' id='thBubble'>
-                    Doubling the number of OJT units in the state
-                    </div>
-
-                </div>
-
-                <div className='row'>
-                    <div className='col-6 semi-circle d-flex flex-column justify-content-center' id='foBubble'>
-                        <div className='row justify-content-center' 
-                        style={{
-                            color:" #FFFFFF",
-                            fontSize: "1.5rem" ,
-                            fontWeight:700
-
-                        }}>
-                            Potentially impacting
-                        </div>
-                        <div className='row justify-content-center'
-                        style={{
-                            color:"#FFB900",
-                            fontSize: "4rem" ,
-                            fontWeight:400
-
-                        }}>
-                            XXXX
-                        </div>
-                        <div className='row justify-content-center' 
-                        style={{
-                            color:" #FFFFFF",
-                            fontSize: "1.5rem",
-                            fontWeight:700
-
-                        }}>
-                            of students
-                        </div>
-                    </div>
-                </div>
-            </div>    
-        </div>
-     </div>
-
-          
-
-        </div> */}
+        </div>:<div></div>}
         
-        </Fragment>
+        </div>
   )
 }
 
