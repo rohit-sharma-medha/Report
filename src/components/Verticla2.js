74db9261b49365d72e633dd2d89b552b90466891
Deepak Sharma <deepak@aayworks.com>
2023-03-22 13:35:16 +0530

    fixed vertical slider issue and orbit issue

--- src/components/Verticla2.js
+++ src/components/Verticla2.js
@@ -166,99 +166,12 @@ export const Verticla2 = () => {
                             </div>
                         </div>
                     </div>
-
-                    <FirstCircleUi className="vertical2-slide d-flex "/>
-                    <SecondCircleUi/>
-                    {/* <div className='vertical2-slide d-flex '>
-                        <div className='col-12'>
-                            <div className='row upper-content-3rdslide'>
-                                <div className='col-5'>
-                                    <p className='uppercontent-head'>Make vocational education aspirational in UP</p>
-                                </div>
-                                <div className='col-6 mx-5'>
-                                    <p className='uppercontent-text'>We renewed our 8-year-old partnership with the Department of Technical Education, Uttar Pradesh (DTE-UP) to strengthen polytechnic education, by:</p>
-                                </div>
-                            </div>
-                            <div className="semi-circle"></div>
-                            <div className='col-12 half-circle-animation'>
-                                <div className='row first-line-content'>
-                                    <div className='col-12 d-flex justify-content-center first-line-content-box'>
-                                        <p className='first-line-content-box-text'>Improving the physical learning facilities,</p>
-                                    </div>
-                                </div>
-                                <div className='row d-flex justify-content-between second-line-content'>
-
-                                    <div className='col-4 px-5'>
-                                        <p className='first-line-content-box-text'>Introducing an industry-relevant curriculum,</p>
-                                    </div>
-                                    <div className='col-4 px-5'>
-                                        <p className='first-line-content-box-text' >Helping industry and academia work closely</p>
-                                    </div>
-
-
-                                </div>
-                                <div className='row d-flex justify-content-around third-line-content'>
-                                    <div className='col-4'>
-                                        <p className='first-line-content-box-text'>Building experiential facilitation skill in teachers,</p>
-                                    </div>
-                                    <div className='col-4'>
-                                        <p className='first-line-content-box-text yellowbackground'>Potentially impact 70,000+ young people</p>
-                                    </div>
-                                    <div className='col-4'>
-                                        <p className='first-line-content-box-text'>Driving data-backed governance in the system</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div>
+                        {currentPageIndex ==3 ?  <FirstCircleUi className="vertical2-slide d-flex "/>:""}
                     </div>
-                    <div className='vertical2-slide d-flex '>
 
-                        <div className='col-12'>
-                            <div className='row upper-content-3rdslide'>
-                                <div className='col-5'>
-                                    <p className='uppercontent-head'>Elevate on-the-job vocational training in Haryana</p>
-                                </div>
-                                <div className='col-6 mx-5'>
-                                    <p className='uppercontent-text'>Over the next 2 years, we will help the Skill <br /> Development and Industrial Training Department,<br /> Haryana (SDIT-Haryana) scale their dual-system of <br /> training model for vocational education students by:</p>
-                                </div>
-                            </div>
-                            <div className="semi-circle1"></div>
-                            <div className='col-12 half-circle-animation'>
-                                <div className='row first-line-content'>
-                                    <div className='col-12 d-flex justify-content-center first-line-content-box'>
-                                        <p className='first-line-content-box-text'>Building strong industry partnerships for quality OJTs</p>
-                                    </div>
-                                </div>
-                                <div className='row d-flex justify-content-between second-line-content'>
-
-                                    <div className='col-4 px-5'>
-                                        <p className='first-line-content-circle-text'>Providing on-the-job trainings (OJT) of 6 months’ duration</p>
-                                    </div>
-                                    <div className='col-4 px-5'>
-                                        <p className='first-line-content-box-text' >Doubling the number of OJT units in the state</p>
-                                    </div>
-
-
-                                </div>
-                                <div className='row d-flex flex-column potential'>
-                                    <p className='potential-text '>Potentially impacting</p>
-                                    <p className='potential-text yellow-text'>XXXX</p>
-                                    <p className='potential-text '>of students</p>
-                                </div>
-                                <div className='row d-flex justify-content-around third-line-content'>
-
-                                    <div className='col-4'>
-                                        <p className='greenbackgraound'></p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div> */}
-
-                    {/* <div className='vertical2-slide d-flex'>6</div> */}
+                    <div>{currentPageIndex ==4 ? <SecondCircleUi/>: ""}</div>
+                
                 </Carousel>
             </div>
 
