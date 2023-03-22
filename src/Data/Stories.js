406e78eafe852881be29a2ea9c148e01a9aaaa2b
Rohit <rohitk7065@gmail.com>
2023-03-22 11:43:18 +0530

    flash effect

--- src/Data/Stories.js
+++ src/Data/Stories.js
@@ -8,36 +8,14 @@ export const Stories = (props) => {
     console.log("props.sub ------------------> \n ", props.sub);
   }, [magicName]);
   return (
-    // <div  className={`d-flex flex-column ${"slide" + props.index}`}  >
-    //   <p className={props.head_class}>{props.head}</p>
-    //   <p className={props.className}>{name}</p>
-    //   <p className={props.empclass}>{props.emp}</p>
-    // </div>
+  
 
     <>
       <div className='stories'>
-        {/* <Typewriter
+        
+        <div className='animate__animated animate__fadeIn time1' dangerouslySetInnerHTML={ { __html: magicName}} />
 
-          onInit={(typewriter) => {
-            typewriter.typeString(props.head)
-              .start();
-          }}
-          options={{
-            delay: 14,
-            
-          }}
-        /> */}
-        <Typewriter
-
-          onInit={(typewriter) => {
-            typewriter.typeString(magicName)
-              .start();
-          }}
-          options={{
-            delay: 14,
-            pauseFor:2000,
-          }}
-        />
+        
       </div>
 
     </>
