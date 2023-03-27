1462d002557b7e8105e0495c2b4123862a90d8ae
Rohit <rohitk7065@gmail.com>
2023-03-27 11:05:16 +0530

    half orbit

--- src/components/Second.js
+++ src/components/Second.js
@@ -2,31 +2,57 @@ import React, {useRef, useState,useEffect } from 'react';
 
 export const Second= () => {
 
-  const [isVisible, setIsVisible] = useState(false);
+  // const [isVisible, setIsVisible] = useState(false);
   
-  const elementRef = useRef(null);
+  // const elementRef = useRef(null);
+  // useEffect(() => {
+  //   // let classtobeAdded ="animate__animated animate__fadeInUp"
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         setIsVisible(true)
+  //         console.log("hello",isVisible)
+  //         // entry.target.classList.add("animate__fadeInUp");
+  //       }
+        
+  //     });
+  //   });
+    
+  //   observer.observe(elementRef.current);
+  //   // console.log("isVisible ",isVisible);
+  //   return () => {
+  //     observer.unobserve(elementRef.current);
+  //   };
+  // }, []);
+
+
+
+  const elementRef = useRef(null)
+  const [ isVisible, setIsVisible ] = useState(false)
+
+  const callbackFunction = (entries) => {
+    const [ entry ] = entries
+    setIsVisible(entry.isIntersecting)
+  }
+  
+
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('example-element-visible');
-        }
-      });
-    });
     
-    observer.observe(elementRef.current);
-    
+    const observer = new IntersectionObserver(callbackFunction)
+    if (elementRef.current) observer.observe(elementRef.current)
+  
     return () => {
-      observer.unobserve(elementRef.current);
-    };
-  }, []);
+      if(elementRef.current) observer.unobserve(elementRef.current)
+    }
+  }, [elementRef])
+
   
   return (
     <>
     
     
-        <div  className='Seond_slide'>
-            <p ref={elementRef} className={`second_slide_text BebasNeue`} style={{}}>
+        <div   className='Seond_slide '>
+            <p ref={elementRef} className={isVisible ? " animate__animated animate__fadeInUp time2  second_slide_text BebasNeue":"font-0"} style={{}}>
             OVER THE PAST DECADE, <span className="yellow-text"> 85,000+</span> PASSIONATE YOUTH HAVE <br /> ENGAGED WITH US TO <span className="yellow-text">START CAREERS THEY ENJOY </span>
             </p>
         </div>
